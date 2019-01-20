8b8323ed3e43d3a2bda87c5d3f22d7258592ac33
friendbear <kumagai@bear.plala.or.jp>
2019-01-20 10:00:36 +0900

    Functional Collections : A functional Set

--- src/main/scala/functionprograming/MySet.scala
+++ src/main/scala/functionprograming/MySet.scala
@@ -1,12 +1,20 @@
-package advanced
+package functionprograming
 
-
+/**
+  * A Taste of Advanced Scala
+  * AdvancedFunctional Programing
+  *
+  * - Functional Collections : A functional Set
+  * - Enhancing A Functional Set
+  */
 trait MySet[A] extends (A => Boolean) {
 
-  // 自身のcontainsメソッドの呼び出し
-  def apply(elem: A): Boolean =
+  def apply(elem: A): Boolean =  // A => Boolean implement. 🔴
     contains(elem)
 
+  /*
+   * EXERCISE #1
+   */
   def contains(elem: A): Boolean
   def +(elem: A): MySet[A]
   def ++(anotherSet: MySet[A]): MySet[A]
@@ -17,16 +25,16 @@ trait MySet[A] extends (A => Boolean) {
   def foreach(f: A=> Unit): Unit
 
   /*
-   * EXECISE #2
+   * EXERCISE #2
    * removing an element
    * intersection whit another set
    * difference with another set
    */
   def -(elem: A): MySet[A]
-  def --(anoterSet: MySet[A]): MySet[A]
+  def --(anotherSet: MySet[A]): MySet[A]
   def &(anotherSet: MySet[A]): MySet[A]
 
-  // EXECISE #3
+  // EXERCISE #3
   // set[1, 2, 3] => implement a unary_! = NEGATION of a set
   def unary_! :MySet[A]
 }
@@ -36,24 +44,24 @@ class EmptySet[A] extends MySet[A] {
   def contains(elem: A): Boolean = false
   def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
   def ++(anotherSet: MySet[A]): MySet[A] = anotherSet
-
   def map[B](f: A => B): MySet[B] = new EmptySet[B]
   def flatMap[B](f: A => MySet[B]): MySet[B] = new EmptySet[B]
   def filter(predicate: A => Boolean): MySet[A] = this
   def foreach(f: A=>Unit): Unit = ()
 
   def -(elem: A): MySet[A] = this
-  def --(anoterSet: MySet[A]): MySet[A] = this
+  def --(anotherSet: MySet[A]): MySet[A] = this
   def &(anotherSet: MySet[A]): MySet[A] = this
   
   def unary_! :MySet[A]  = ???
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
-  def contains(elem: A): Boolean = 
+
+  def contains(elem: A): Boolean =   // 🔴
     elem == head || tail.contains(elem)
 
-  def +(elem: A): MySet[A] = 
+  def +(elem: A): MySet[A] =  // 🔴
     if (this contains elem) this
     else new NonEmptySet[A] (elem, this)
 
@@ -64,12 +72,12 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
    * [] ++ [4 5] + 1 + 2 + 3
    * [4 5] + 1 + 2 + 3 + = [4 5 1 2 3]
    */
-  def ++(anotherSet: MySet[A]): MySet[A] = 
+  def ++(anotherSet: MySet[A]): MySet[A] =  // 🔴
     tail ++ anotherSet + head
 
-  def map[B](f: A => B): MySet[B] = (tail map f) + f(head)
-  def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head)
-  def filter(predicate: A => Boolean): MySet[A] = {
+  def map[B](f: A => B): MySet[B] = (tail map f) + f(head) // 🔴
+  def flatMap[B](f: A => MySet[B]): MySet[B] = (tail flatMap f) ++ f(head) // 🔴
+  def filter(predicate: A => Boolean): MySet[A] = { // 🔴
     val filteredTail = tail filter predicate
     if (predicate(head)) filteredTail + head
     else filteredTail
@@ -80,9 +88,9 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     tail foreach f
   }
 
-  // part 2
+  // part #2
   def -(elem: A): MySet[A] =
-    if ( head == elem) tail
+    if (head == elem) tail
     else tail - elem + head
 
   def --(anotherSet: MySet[A]): MySet[A] = filter(x => !anotherSet(x))
@@ -92,9 +100,14 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
   def unary_! : MySet[A] = ???
 }
 object MySet {
-
+  /*
+    val s = MySet(1,2,3) = buildSet(seq(1,2,3), [])
+    = buildSet(seq(2,3, [] + 1)
+    = buildSet(seq(3), [1] + 2)
+    = buildSet(seq(), [1, 2] + 3)
+    = [1, 2, 3]
+   */
   def apply[A](values: A*): MySet[A] = {
-
     @annotation.tailrec
     def buildSet(valSeq: Seq[A], acc: MySet[A]): MySet[A] =
       if (valSeq.isEmpty) acc
@@ -105,5 +118,14 @@ object MySet {
 
 object MySetPlayground extends App {
   val s = MySet(1, 2, 3, 4)
-  s + 5 ++ MySet(-1, -2) + 3 flatMap(x =>MySet(x, 10 * x)) filter(_ % 2 == 0) foreach println
+
+  // EXERCISE #1
+  // test 1
+  s foreach println
+
+  // test 2
+  s + 5 ++ MySet(-1, -2) + 3 flatMap(x => MySet(x, 10 * x)) foreach println
+
+  // test 3
+  s + 5 ++ MySet(-1, -2) + 3 flatMap(x => MySet(x, 10 * x)) filter(_ % 2 == 0) foreach println
 }
